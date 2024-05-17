a8dd4c730ffe0cd97c36e29eed56193ae7dfdc73
E213634L <enzo.bertel@etu.univ-nantes.fr>
2024-05-17 21:41:30 +0200

    map

---
+++ isometric_tutorial/Map.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Map:
+    def __init__(self,file) -> None:
+        self.file = file
+        
+    def draw(self,display):
+        grass_img = pygame.image.load('./images/grass.png').convert()
+        grass_img.set_colorkey((0, 0, 0))
+
+        f = open(self.file)
+
+
+        map_data = [[int(c) for c in row] for row in f.read().split('\n')]
+        f.close()
+
+        for y, row in enumerate(map_data):
+            for x, tile in enumerate(row):
+                if tile:
+                    #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
+                    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
--- isometric_tutorial/main.py
+++ isometric_tutorial/main.py
@@ -1,4 +1,5 @@
 import pygame, sys, time, random
+from Map import Map
 
 from pygame.locals import *
 pygame.init()
@@ -10,30 +11,19 @@ screen = pygame.display.set_mode((900, 900),0,32)
 display = pygame.Surface((900, 900))
 mouse = pygame.mouse
 
-grass_img = pygame.image.load('grass.png').convert()
-grass_img.set_colorkey((0, 0, 0))
+# chateau_img = pygame.image.load('image.png').convert()
+# chateau_img.set_colorkey((255, 255, 255))
+# size = chateau_img.get_size()
+# coeff = 0.5
+# chateau_img = pygame.transform.scale(chateau_img,(size[0]*coeff,size[1]*coeff))
 
-chateau_img = pygame.image.load('image.png').convert()
-chateau_img.set_colorkey((255, 255, 255))
-size = chateau_img.get_size()
-coeff = 0.5
-chateau_img = pygame.transform.scale(chateau_img,(size[0]*coeff,size[1]*coeff))
-
-f = open('map.txt')
-map_data = [[int(c) for c in row] for row in f.read().split('\n')]
-f.close()
+map = Map("./config/map.txt")    
 
 while True:
     #display.fill((0,0,0))
     (mouse_x, mouse_y) = mouse.get_pos()
 
-    
-    for y, row in enumerate(map_data):
-        for x, tile in enumerate(row):
-            if tile:
-                #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
-                display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
-    display.blit(chateau_img,(0,0))           
+    map.draw(display)
 
     for event in pygame.event.get():
         if event.type == QUIT:
